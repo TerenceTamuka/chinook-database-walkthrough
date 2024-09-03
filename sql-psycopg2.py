e378965615de9639e4587fecc9211e0518cc0f68
Terence Zengeya <terencetamuka@gmail.com>
2024-09-03 13:44:44 +0000

    Used an expression language to run same queries as psycopg2

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -10,11 +10,24 @@ cursor = connection.cursor()
 #cursor.execute('SELECT * FROM "Artist"')
 
 # Query 2 - Select only the "Name" column from the "Artist" table
-cursor.execute('SELECT "Name" FROM "Artist"')
+#cursor.execute('SELECT "Name" FROM "Artist"')
 
 # Query 3 select only "Queen" from the "Artist" table
-cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+#cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 #cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ('Queen',))
+
+# Query 4 - Select only by "ArtistId" #51 from the "Artist" table
+#cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s',[51])
+#cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', (51,))
+
+# Query 5- select only the albums with "ArtistId" #51 on the "Album" table
+#cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6- select all tracks where the composer is "Queen" from the "Track" table
+#cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Query 7 - select a differnt (exisiting) composer from the "Composer" table
+cursor.execute('SELECT * FROM "Composer" WHERE "AlbumId" = %s', ["Album"])
 
 #fetch the results (multiple)
 #results = cursor.fetchall()
